cb707e9bbff73c88da26a1c8ee3a83e45a638b7b
JoaoGFarias <joao@thatsabug.com>
2026-04-22 20:45:59 +0200

    Add domain records to dsl/domain

---
+++ java-automation/src/test/java/com/lineasupply/automation/dsl/domain/CartItem.java
@@ -0,0 +1,3 @@
+package com.lineasupply.automation.dsl.domain;
+
+public record CartItem(String title) {}
---
+++ java-automation/src/test/java/com/lineasupply/automation/dsl/domain/ProductCard.java
@@ -0,0 +1,3 @@
+package com.lineasupply.automation.dsl.domain;
+
+public record ProductCard(String title, String price, String imageUrl) {}
